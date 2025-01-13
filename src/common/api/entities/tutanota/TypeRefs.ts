e40827828e18920d5cc406aea520e58e64c22080
Kinan <104761667+kibibytium@users.noreply.github.com>
2025-01-13 14:54:33 +0100

    generated types for TutanotaModelV80

--- src/common/api/entities/tutanota/TypeRefs.ts
+++ src/common/api/entities/tutanota/TypeRefs.ts
@@ -1095,8 +1095,29 @@ export function createImportMailGetIn(values: StrippedEntity<ImportMailGetIn>): 
 
 export type ImportMailGetIn = {
 	_type: TypeRef<ImportMailGetIn>;
+	_errors: Object;
 
 	_format: NumberString;
+	newImportedMailSetName: string;
+	ownerEncSessionKey: Uint8Array;
+	ownerGroup: Id;
+	ownerKeyVersion: NumberString;
+	totalMails: NumberString;
+
+	targetMailFolder: IdTuple;
+}
+export const ImportMailGetOutTypeRef: TypeRef<ImportMailGetOut> = new TypeRef("tutanota", "ImportMailGetOut")
+
+export function createImportMailGetOut(values: StrippedEntity<ImportMailGetOut>): ImportMailGetOut {
+	return Object.assign(create(typeModels.ImportMailGetOut, ImportMailGetOutTypeRef), values)
+}
+
+export type ImportMailGetOut = {
+	_type: TypeRef<ImportMailGetOut>;
+
+	_format: NumberString;
+
+	mailState: IdTuple;
 }
 export const ImportMailPostInTypeRef: TypeRef<ImportMailPostIn> = new TypeRef("tutanota", "ImportMailPostIn")
 
@@ -1106,17 +1127,11 @@ export function createImportMailPostIn(values: StrippedEntity<ImportMailPostIn>)
 
 export type ImportMailPostIn = {
 	_type: TypeRef<ImportMailPostIn>;
-	_errors: Object;
 
 	_format: NumberString;
-	newImportedMailSetName: string;
-	ownerEncSessionKey: Uint8Array;
-	ownerGroup: Id;
-	ownerKeyVersion: NumberString;
 
 	encImports: StringWrapper[];
 	mailState: IdTuple;
-	targetMailFolder: IdTuple;
 }
 export const ImportMailPostOutTypeRef: TypeRef<ImportMailPostOut> = new TypeRef("tutanota", "ImportMailPostOut")
 
@@ -1128,8 +1143,6 @@ export type ImportMailPostOut = {
 	_type: TypeRef<ImportMailPostOut>;
 
 	_format: NumberString;
-
-	mailState: IdTuple;
 }
 export const ImportMailStateTypeRef: TypeRef<ImportMailState> = new TypeRef("tutanota", "ImportMailState")
 
@@ -1147,6 +1160,7 @@ export type ImportMailState = {
 	failedMails: NumberString;
 	status: NumberString;
 	successfulMails: NumberString;
+	totalMails: NumberString;
 
 	importedMails: Id;
 	targetFolder: IdTuple;
--- tuta-sdk/rust/sdk/src/entities/generated/tutanota.rs
+++ tuta-sdk/rust/sdk/src/entities/generated/tutanota.rs
@@ -1385,6 +1385,15 @@ impl Entity for ImportMailDataMailReference {
 #[cfg_attr(any(test, feature = "testing"), derive(PartialEq, Debug))]
 pub struct ImportMailGetIn {
 	pub _format: i64,
+	pub newImportedMailSetName: String,
+	#[serde(with = "serde_bytes")]
+	pub ownerEncSessionKey: Vec<u8>,
+	pub ownerGroup: GeneratedId,
+	pub ownerKeyVersion: i64,
+	pub totalMails: i64,
+	pub targetMailFolder: IdTupleGenerated,
+	pub _errors: Option<Errors>,
+	pub _finalIvs: HashMap<String, FinalIv>,
 }
 
 impl Entity for ImportMailGetIn {
@@ -1398,18 +1407,26 @@ impl Entity for ImportMailGetIn {
 
 #[derive(uniffi::Record, Clone, Serialize, Deserialize)]
 #[cfg_attr(any(test, feature = "testing"), derive(PartialEq, Debug))]
+pub struct ImportMailGetOut {
+	pub _format: i64,
+	pub mailState: IdTupleGenerated,
+}
+
+impl Entity for ImportMailGetOut {
+	fn type_ref() -> TypeRef {
+		TypeRef {
+			app: "tutanota",
+			type_: "ImportMailGetOut",
+		}
+	}
+}
+
+#[derive(uniffi::Record, Clone, Serialize, Deserialize)]
+#[cfg_attr(any(test, feature = "testing"), derive(PartialEq, Debug))]
 pub struct ImportMailPostIn {
 	pub _format: i64,
-	pub newImportedMailSetName: String,
-	#[serde(with = "serde_bytes")]
-	pub ownerEncSessionKey: Vec<u8>,
-	pub ownerGroup: GeneratedId,
-	pub ownerKeyVersion: i64,
 	pub encImports: Vec<super::sys::StringWrapper>,
 	pub mailState: IdTupleGenerated,
-	pub targetMailFolder: IdTupleGenerated,
-	pub _errors: Option<Errors>,
-	pub _finalIvs: HashMap<String, FinalIv>,
 }
 
 impl Entity for ImportMailPostIn {
@@ -1425,7 +1442,6 @@ impl Entity for ImportMailPostIn {
 #[cfg_attr(any(test, feature = "testing"), derive(PartialEq, Debug))]
 pub struct ImportMailPostOut {
 	pub _format: i64,
-	pub mailState: IdTupleGenerated,
 }
 
 impl Entity for ImportMailPostOut {
@@ -1447,6 +1463,7 @@ pub struct ImportMailState {
 	pub failedMails: i64,
 	pub status: i64,
 	pub successfulMails: i64,
+	pub totalMails: i64,
 	pub importedMails: GeneratedId,
 	pub targetFolder: IdTupleGenerated,
 }
